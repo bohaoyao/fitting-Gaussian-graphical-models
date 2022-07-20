df94250da03d5e1e98fd87352b8a5f7a7715a9c4
Bohao Yao <bohao.yao@cantab.net>
2022-07-20 17:34:24 +0100

    Update Examples 4.8, 4.10.R

--- Examples/Examples 4.8, 4.10.R
+++ Examples/Examples 4.8, 4.10.R
@@ -63,6 +63,28 @@ Ex <- ecdf(qchisq(seq(0,0.9999,0.0001), df=1))
 plot(chisq1, verticals=TRUE, do.points=FALSE)
 plot(Ex, verticals=TRUE, do.points=FALSE, col='red', add=TRUE)
 
+# re-run for RICF
+
+set.seed(1)
+
+n <- 20
+N <- 1000
+chisq <- c()
+
+for (i in 1:N) {
+  Z <- matrix(rnorm(4*n),n,4)
+  X <- Z %*% U
+  out <- ricf(t(LambdaA), Omega, t(X))
+  S <- t(X) %*% X/n
+  chisq[i] <- 2 * (loglikelihoodS(S, S, n) - loglikelihoodS(out$SigmaHat, S, n)) 
+}
+
+ks.test(chisq, pchisq, df=1)
+chisq1 <- ecdf(chisq)
+Ex <- ecdf(qchisq(seq(0,0.9999,0.0001), df=1))
+plot(chisq1, verticals=TRUE, do.points=FALSE)
+plot(Ex, verticals=TRUE, do.points=FALSE, col='red', add=TRUE)
+
 # n = 10
 
 set.seed(1)
@@ -84,3 +106,49 @@ chisq1 <- ecdf(chisq)
 Ex <- ecdf(qchisq(seq(0,0.9999,0.0001), df=1))
 plot(chisq1, verticals=TRUE, do.points=FALSE)
 plot(Ex, verticals=TRUE, do.points=FALSE, col='red', add=TRUE)
+
+# re-run for RICF
+
+set.seed(1)
+
+n <- 10
+N <- 1000
+chisq <- c()
+
+for (i in 1:N) {
+  Z <- matrix(rnorm(4*n),n,4)
+  X <- Z %*% U
+  out <- ricf(t(LambdaA), Omega, t(X))
+  S <- t(X) %*% X/n
+  chisq[i] <- 2 * (loglikelihoodS(S, S, n) - loglikelihoodS(out$SigmaHat, S, n)) 
+}
+
+ks.test(chisq, pchisq, df=1)
+chisq1 <- ecdf(chisq)
+Ex <- ecdf(qchisq(seq(0,0.9999,0.0001), df=1))
+plot(chisq1, verticals=TRUE, do.points=FALSE)
+plot(Ex, verticals=TRUE, do.points=FALSE, col='red', add=TRUE)
+
+#comparison tests
+
+set.seed(1)
+n <- 10
+for (i in 1:20) {
+  Z <- matrix(rnorm(4*n),n,4)
+  X <- Z %*% U
+  S1 <- fitGaussianGraph(X=X, LambdaA=LambdaA, OmegaA=OmegaA, learning_rate = 10^-1, convergence_rate = 10^-12)$Sigmahat
+  S2 <- ricf(t(LambdaA), OmegaA, t(X))$SigmaHat
+  S <- t(X) %*% X/n
+  print(llhd(S1, S, n)-llhd(S2, S, n))
+}
+
+set.seed(1)
+n <- 1000
+for (i in 1:20) {
+  Z <- matrix(rnorm(4*n),n,4)
+  X <- Z %*% U
+  S1 <- fitGaussianGraph(X=X, LambdaA=LambdaA, OmegaA=OmegaA, learning_rate = 10^-1, convergence_rate = 10^-12)$Sigmahat
+  S2 <- ricf(t(LambdaA), OmegaA, t(X))$SigmaHat
+  S <- t(X) %*% X/n
+  print(llhd(S1, S, n)-llhd(S2, S, n))
+}
